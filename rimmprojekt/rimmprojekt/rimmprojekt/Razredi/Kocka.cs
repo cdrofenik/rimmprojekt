fd701b7cddd93defef21d690b901092e3f5547dd
ziga.spindler <ziga.spindler@7981ba2d-c91b-37b3-4c22-2994cee20ce3>
2011-01-02 17:49:55 +0000

    dodan razred Tezej.cs(trenutno deduje od Kocka), kamera sledi objektu

--- rimmprojekt/rimmprojekt/rimmprojekt/Razredi/Kocka.cs
+++ rimmprojekt/rimmprojekt/rimmprojekt/Razredi/Kocka.cs
@@ -15,57 +15,58 @@ namespace rimmprojekt.Razredi
 {
     class Kocka : IDraw
     {
-        private Matrix polozaj;
-        private readonly IVertices vertices;
-        private readonly IIndices indices;
-        private IShader shader;
+        protected Matrix matrix;
+        //public Vector3 polozaj;
+        protected readonly IVertices vertices;
+        protected readonly IIndices indices;
+        protected IShader shader;
 
         public Kocka(float x, float y, float z)
         {
-            polozaj = Matrix.CreateTranslation(new Vector3(x, y, z));
+            matrix = Matrix.CreateTranslation(new Vector3(x, y, z));
 
             Vector2 topLeft = new Vector2(0.0f, 0.0f);
-            Vector2 topRight = new Vector2(1.0f, 0.0f);
-            Vector2 bottomLeft = new Vector2(0.0f, 1.0f);
-            Vector2 bottomRight = new Vector2(1.0f, 1.0f);
+            Vector2 topRight = new Vector2(10.0f, 0.0f);
+            Vector2 bottomLeft = new Vector2(0.0f, 10.0f);
+            Vector2 bottomRight = new Vector2(10.0f, 10.0f);
 
             VertexPositionNormalTexture[] ploskve = new VertexPositionNormalTexture[]
             {
                 // Front Surface
-                new VertexPositionNormalTexture(new Vector3(-1.0f, -1.0f, 1.0f), Vector3.Backward, bottomLeft),
-                new VertexPositionNormalTexture(new Vector3(-1.0f, 1.0f, 1.0f), Vector3.Backward, topLeft), 
-                new VertexPositionNormalTexture(new Vector3(1.0f, -1.0f, 1.0f), Vector3.Backward, bottomRight),
-                new VertexPositionNormalTexture(new Vector3(1.0f, 1.0f, 1.0f), Vector3.Backward, topRight),  
+                new VertexPositionNormalTexture(new Vector3(-10.0f, -10.0f, 10.0f), Vector3.Backward, bottomLeft),
+                new VertexPositionNormalTexture(new Vector3(-10.0f, 10.0f, 10.0f), Vector3.Backward, topLeft), 
+                new VertexPositionNormalTexture(new Vector3(10.0f, -10.0f, 10.0f), Vector3.Backward, bottomRight),
+                new VertexPositionNormalTexture(new Vector3(10.0f, 10.0f, 10.0f), Vector3.Backward, topRight),  
 
                 // Back Surface
-                new VertexPositionNormalTexture(new Vector3(1.0f, -1.0f, -1.0f), Vector3.Forward, bottomLeft),
-                new VertexPositionNormalTexture(new Vector3(1.0f, 1.0f, -1.0f), Vector3.Forward, topLeft), 
-                new VertexPositionNormalTexture(new Vector3(-1.0f, -1.0f, -1.0f), Vector3.Forward, bottomRight),
-                new VertexPositionNormalTexture(new Vector3(-1.0f, 1.0f, -1.0f), Vector3.Forward, topRight), 
+                new VertexPositionNormalTexture(new Vector3(10.0f, -10.0f, -10.0f), Vector3.Forward, bottomLeft),
+                new VertexPositionNormalTexture(new Vector3(10.0f, 10.0f, -10.0f), Vector3.Forward, topLeft), 
+                new VertexPositionNormalTexture(new Vector3(-10.0f, -10.0f, -10.0f), Vector3.Forward, bottomRight),
+                new VertexPositionNormalTexture(new Vector3(-10.0f, 10.0f, -10.0f), Vector3.Forward, topRight), 
 
                 // Left Surface
-                new VertexPositionNormalTexture(new Vector3(-1.0f, -1.0f, -1.0f), Vector3.Left, bottomLeft),
-                new VertexPositionNormalTexture(new Vector3(-1.0f, 1.0f, -1.0f), Vector3.Left, topLeft),
-                new VertexPositionNormalTexture(new Vector3(-1.0f, -1.0f, 1.0f), Vector3.Left, bottomRight),
-                new VertexPositionNormalTexture(new Vector3(-1.0f, 1.0f, 1.0f), Vector3.Left, topRight),
+                new VertexPositionNormalTexture(new Vector3(-10.0f, -10.0f, -10.0f), Vector3.Left, bottomLeft),
+                new VertexPositionNormalTexture(new Vector3(-10.0f, 10.0f, -10.0f), Vector3.Left, topLeft),
+                new VertexPositionNormalTexture(new Vector3(-10.0f, -10.0f, 10.0f), Vector3.Left, bottomRight),
+                new VertexPositionNormalTexture(new Vector3(-10.0f, 10.0f, 10.0f), Vector3.Left, topRight),
 
                 // Right Surface
-                new VertexPositionNormalTexture(new Vector3(1.0f, -1.0f, 1.0f), Vector3.Right, bottomLeft),
-                new VertexPositionNormalTexture(new Vector3(1.0f, 1.0f, 1.0f), Vector3.Right, topLeft),
-                new VertexPositionNormalTexture(new Vector3(1.0f, -1.0f, -1.0f), Vector3.Right, bottomRight),
-                new VertexPositionNormalTexture(new Vector3(1.0f, 1.0f, -1.0f), Vector3.Right, topRight),
+                new VertexPositionNormalTexture(new Vector3(10.0f, -10.0f, 10.0f), Vector3.Right, bottomLeft),
+                new VertexPositionNormalTexture(new Vector3(10.0f, 10.0f, 10.0f), Vector3.Right, topLeft),
+                new VertexPositionNormalTexture(new Vector3(10.0f, -10.0f, -10.0f), Vector3.Right, bottomRight),
+                new VertexPositionNormalTexture(new Vector3(10.0f, 10.0f, -10.0f), Vector3.Right, topRight),
 
                 // Top Surface
-                new VertexPositionNormalTexture(new Vector3(-1.0f, 1.0f, 1.0f), Vector3.Up, bottomLeft),
-                new VertexPositionNormalTexture(new Vector3(-1.0f, 1.0f, -1.0f), Vector3.Up, topLeft),
-                new VertexPositionNormalTexture(new Vector3(1.0f, 1.0f, 1.0f), Vector3.Up, bottomRight),
-                new VertexPositionNormalTexture(new Vector3(1.0f, 1.0f, -1.0f), Vector3.Up, topRight),
+                new VertexPositionNormalTexture(new Vector3(-10.0f, 10.0f, 10.0f), Vector3.Up, bottomLeft),
+                new VertexPositionNormalTexture(new Vector3(-10.0f, 10.0f, -10.0f), Vector3.Up, topLeft),
+                new VertexPositionNormalTexture(new Vector3(10.0f, 10.0f, 10.0f), Vector3.Up, bottomRight),
+                new VertexPositionNormalTexture(new Vector3(10.0f, 10.0f, -10.0f), Vector3.Up, topRight),
 
                 // Bottom Surface
-                new VertexPositionNormalTexture(new Vector3(-1.0f, -1.0f, -1.0f), Vector3.Down, bottomLeft),
-                new VertexPositionNormalTexture(new Vector3(-1.0f, -1.0f, 1.0f), Vector3.Down, topLeft),
-                new VertexPositionNormalTexture(new Vector3(1.0f, -1.0f, -1.0f), Vector3.Down, bottomRight),
-                new VertexPositionNormalTexture(new Vector3(1.0f, -1.0f, 1.0f), Vector3.Down, topRight)
+                new VertexPositionNormalTexture(new Vector3(-10.0f, -10.0f, -10.0f), Vector3.Down, bottomLeft),
+                new VertexPositionNormalTexture(new Vector3(-10.0f, -10.0f, 10.0f), Vector3.Down, topLeft),
+                new VertexPositionNormalTexture(new Vector3(10.0f, -10.0f, -10.0f), Vector3.Down, bottomRight),
+                new VertexPositionNormalTexture(new Vector3(10.0f, -10.0f, 10.0f), Vector3.Down, topRight)
             };
 
             short[] boxIndices = new short[]
@@ -84,17 +85,25 @@ namespace rimmprojekt.Razredi
             ploskve = null;
             boxIndices = null;
 
-            MaterialShader material = new MaterialShader();
-            material.SpecularColour = Color.LightYellow.ToVector3();
-            shader = material;
+            //MaterialShader material = new MaterialShader();
+            //material.SpecularColour = Color.LightYellow.ToVector3();//with a nice sheen
+
+            //Vector3 lightDirection = new Vector3(0.5f, 1, -0.5f); //a less dramatic direction
+
+            //MaterialLightCollection lights = new MaterialLightCollection();
+            //lights.AmbientLightColour = Color.White.ToVector3() * 0.5f;
+            //lights.CreateDirectionalLight(lightDirection, Color.White);
+
+            //material.LightCollection = lights;
+
+            //shader = material;
         }
 
-        public void Draw(DrawState state)
+        virtual public void Draw(DrawState state)
         {
             //draw the vertices as a triangle list, with the indices
-            using (state.WorldMatrix.PushMultiply(ref polozaj))
+            using (state.WorldMatrix.PushMultiply(ref matrix))
             {
-
                 //cull test the custom geometry
                 if (CullTest(state))
                 {
