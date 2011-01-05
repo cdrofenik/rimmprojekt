3058a38a7206fe4349e3020dac75e2322fb2c783
c3rtz0r <c3rtz0r@7981ba2d-c91b-37b3-4c22-2994cee20ce3>
2011-01-05 20:02:14 +0000

    Dodan prikaz igralca oz. njegovo stanje in prikaz inventorija s pritiskom tipke I.

---
+++ trunk/rimmprojekt/rimmprojekt/rimmprojekt/Razredi/Oprema.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+using Xen;
+using Xen.Camera;
+using Xen.Graphics;
+using Xen.Ex.Geometry;
+using Xen.Ex.Material;
+
+namespace rimmprojekt.Razredi
+{
+    class Oprema
+    {
+        protected String imeOpreme;
+        protected String tipOpreme;
+        protected String damageOpreme;
+        protected String armorOpreme;
+        protected Vector2 velikostOpreme;
+        protected Texture2D slikaOpreme;
+
+        public Oprema(String ime, String tip, String damage, String armor, String imgReference)
+        {
+            this.imeOpreme = ime;
+            this.tipOpreme = tip;
+            this.damageOpreme = damage;
+            this.armorOpreme = armor;
+        }
+    }
+}
